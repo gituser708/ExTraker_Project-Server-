a296d0387220294399af78bbc34efdb082d77679
gituser708 <donmania999@gmail.com>
2025-08-04 20:24:11 +0530

    push

--- middlewares/verifyCookie.js
+++ middlewares/verifyCookie.js
@@ -4,7 +4,13 @@ const User = require('../model/User');
 
 const verifyCookie = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
+    // Check cookie first, then Authorization header
+    const token = 
+      req.cookies.token || 
+      (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer'
+        ? req.headers.authorization.split(' ')[1]
+        : null);
+
     if (!token) {
       return res.status(401).json({ message: 'No token found' });
     }
