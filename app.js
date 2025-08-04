5c8de1af800c42cc4fdc43beb155f37a73994e54
gituser708 <donmania999@gmail.com>
2025-08-04 20:31:31 +0530

    commit

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const app = express();
 app.use(cors({
     origin: 'https://extracker-web-app.onrender.com',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 
